1b6fe7404b7478ceabefec98fc314f1213cf6d48
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2017-05-13 18:02:16 -0300

    Inicio da implementação da rota para salvar as transações de compra de ações no banco de dados.

--- app/infra/carteirasDAO.js
+++ app/infra/carteirasDAO.js
@@ -3,7 +3,14 @@ function CarteirasDAO(connection) {
 }
 
 CarteirasDAO.prototype.lista = function (callback) {
-    this._connection.query('select * from shares', callback);
+    this._connection.query('select * from transection_shares', callback);
+}
+
+CarteirasDAO.prototype.salva = function (carteira, callback) {
+    // gravar no banco todos os TICKERS para conseguiur gravar corretamente
+    // this._connection.query('insert into shares (ticker, company_name, sector) values (?, ?, ?)', [carteira.ticker, carteira.company_name, carteira.sector],callback);
+    //
+    // this._connection.query('insert into buy_shares (date_buy, quantity, price) values (?, ?, ?)', [carteira.date_buy, carteira.quantity, carteira.price],callback);
 }
 
 module.exports = function () {
--- app/routes/carteiras.js
+++ app/routes/carteiras.js
@@ -1,19 +1,30 @@
 module.exports = function (app) {
-    app.get('/carteira/lista', function (req, res) {
+
+    var listaCarteiras = function (req, res) {
 
         var connection = app.infra.connectionFactory();
         var carteirasDAO = new app.infra.carteirasDAO(connection);
 
         carteirasDAO.lista(function (err, results) {
-            res.render('carteiras/lista', {
+            res.render('carteiras/acoes', {
                 lista: results
             });
         });
+        connection.end();
+}
 
+    app.get('/carteiras', listaCarteiras);
+
+    app.post('/carteiras', function (req, res) {
+
+        var carteira = req.body;
+        console.log(carteira);
+        var connection = app.infra.connectionFactory();
+        var carteirasDAO = new app.infra.carteirasDAO(connection);
+
+        carteirasDAO.salva(carteira, function (err, results) {
+            res.redirect('/carteiras');
+        });
         connection.end();
     });
-
-    // app.get('/carteira/acoes', function (req, res) {
-    //     res.render('carteiras/acoes');
-    // });
 }
