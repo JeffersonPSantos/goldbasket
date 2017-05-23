4183bbd19bc72b23ee46059a23d7182c408138b5
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2017-05-23 15:15:31 -0300

    config banco de dados para deploy no heroku

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -10,13 +10,25 @@ function createDBConnection() {
                 });
     }
 
-    if(process.env.NODE_ENV == 'test')
+    if(process.env.NODE_ENV == 'test'){
         return mysql.createConnection({
                     host: 'localhost',
                     user: 'admin',
                     password: '',
                     database: 'goldbasket_test'
                 });
+    }
+
+    if (process.env.NODE_ENV == 'production') {
+        var url = process.env.CLEARDB_DATABASE_URL;
+        var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
+        return mysql.createConnection({
+            host: grupo[3];
+            user: grupo[1];
+            password: grupo[2];
+            database: grupo[4];
+        });
+    }
 }
 
 module.exports = function () {
